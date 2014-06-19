c53e84520b6550053fcd1b7fee9fe94932c60ad0
shail27385 <shaileshkumar27@gmail.com>
2014-06-19 15:30:59 +0530

    Assignment 3 - Function to cache inverse of Matrix
    Function created to cache the inverse of Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function - Cache the inverse of Matrix
+# makeCacheMatrix 
+# list of function
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the value of inverse of the matrix
+# 4. Get the value of inverse of the matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mydata = matrix()) 
+{
+  inv <- NULL
+  setdata <- function(y) 
+  {
+    mydata <<- y
+    inv <<- NULL
+  }
+  getdata <- function() mydata
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(setdata=setdata, getdata=getdata, setinverse=setinverse, getinverse=getinverse)
 }
 
+# function returning the inverse of the matrix.
+# If the inverse has already been computed, get the result and skips the computation.
+# If not, it computes the inverse, sets the value in the cache via setinverse function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mydata) 
+{
+  inv <- mydata$getinverse()
+  if(!is.null(inv)) 
+  {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- mydata$getdata()
+  inv <- solve(data)
+  mydata$setinverse(inv)
+  inv
 }
+
+######################## Dry run  ###########################
+
+## x = rbind(c(1,4,1,1), c(1,4,0,1), c(2,3,1,2), c(3,2,6,4))
+## m = makeCacheMatrix(x)
+
+## Get the Data of the Matrix
+## m$getdata()
+
+##    [,1] [,2] [,3] [,4]
+##[1,]    1    4    1    1
+##[2,]    1    4    0    1
+##[3,]    2    3    1    2
+##[4,]    3    2    6    4
+
+## No cache in the first run
+## cacheSolve(m)
+##    [,1] [,2] [,3] [,4]
+##[1,]  3.2 -4.8  2.8   -1
+##[2,]  0.2  0.2 -0.2    0
+##[3,]  1.0 -1.0  0.0    0
+##[4,] -4.0  5.0 -2.0    1
+
+## Retrieving from the cache in the second run
+## cacheSolve(m)
+
+##getting cached data.
+##    [,1] [,2] [,3] [,4]
+##[1,]  3.2 -4.8  2.8   -1
+##[2,]  0.2  0.2 -0.2    0
+##[3,]  1.0 -1.0  0.0    0
+##[4,] -4.0  5.0 -2.0    1
